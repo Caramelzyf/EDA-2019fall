4b62b4e3e286bb768e22be461557a5f91c4f2406
yfwonder <43399235+yfwonder@users.noreply.github.com>
2019-12-02 01:52:46 +0800

    Update cnt16.v
    &sub

--- 5-11/cnt16.v
+++ 5-11/cnt16.v
@@ -1,5 +1,6 @@
-module cnt16(clk,rst,en,cout,dout);
-  input clk,en,rst;
+
+module cnt16(clk,rst,en,cout,dout,add);
+  input clk,en,rst,add;
   output [15:0] dout;
 
   output cout;
@@ -8,17 +9,29 @@ module cnt16(clk,rst,en,cout,dout);
 
 always@(posedge clk or negedge rst)
   begin
-	if(!rst) data<=0;
-	else if(en)
-	begin
-	  if(data<65535) data<=data+1;
-	  else data<=16'h0000;
-	end
+    if(!rst) data<=0;
+    else if(en)
+  begin
+  if(add==1)
+    begin
+    if(data<65535) data<=data+1;
+    else data<=16'h0000;
+    end
+  else if(add==0)
+    begin
+    if(data!=0) data<=data-1;
+    else data<=16'hffff;
+    end
+  end
   end
 always@(data)
-begin
-  if(data==16'hffff) cout=1'b1;
-  else cout=1'b0;
-end
+  begin
+    if(add==1)
+  begin
+      if(data==16'hffff) cout=1'b1;
+      else cout=1'b0;
+  end
+    else if(add==0)  cout=1'b0;
+  end
 assign dout=data;
 endmodule
